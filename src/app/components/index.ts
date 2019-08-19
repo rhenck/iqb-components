5df06cf82fa5fc05354b5f0f65cdab26530545bc
paflov <paf@titelfrei.de>
2019-08-19 15:46:33 +0200

    clean up index.ts files

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,3 @@
-export * from './dialogs/confirm/confirm-dialog.component';
-export * from './dialogs/message/message-dialog.component';
-export * from './pipes/bytes.pipe';
+export { ConfirmDialogComponent, ConfirmDialogData } from './dialogs/confirm/confirm-dialog.component';
+export { MessageDialogComponent, MessageDialogData, MessageType } from './dialogs/message/message-dialog.component';
+export { BytesPipe } from './pipes/bytes.pipe';
