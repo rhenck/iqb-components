1632c3fffaca9d5193dc5736e65673420c2d81d2
paflov <paf@titelfrei.de>
2019-08-21 16:14:14 +0200

    include barrel file to make components usable in comonent using projects

---
+++ src/app/components/public_api.ts
@@ -0,0 +1,4 @@
+export * from './iqb-components.module';
+export * from './dialogs/confirm/confirm-dialog.component';
+export * from './dialogs/message/message-dialog.component';
+export * from './pipes/bytes.pipe';
