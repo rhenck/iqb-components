2d18437c1c97ccac106780fb6bcd7cfa5e5b1181
mechtelm <mechtel@iqb.hu-berlin.de>
2020-01-28 18:45:59 +0100

    deleted: reference to BrowserAnimationsModule

--- src/app/components/iqb-components.module.ts
+++ src/app/components/iqb-components.module.ts
@@ -1,8 +1,6 @@
 import { MatDialogModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { ConfirmDialogComponent } from './dialogs/confirm/confirm-dialog.component';
 import { MessageDialogComponent } from './dialogs/message/message-dialog.component';
 import { BytesPipe } from './pipes/bytes.pipe';
@@ -15,8 +13,7 @@ import {ErrorHandler, ServerError} from "./iqb-components.classes";
     CommonModule,
     MatDialogModule,
     MatIconModule,
-    MatButtonModule,
-    BrowserAnimationsModule
+    MatButtonModule
   ],
   entryComponents: [
     ConfirmDialogComponent,
