221c1e963c40ca83c28f993301a32be8e4c54d88
mechtelm <mechtel@iqb.hu-berlin.de>
2020-02-13 17:38:18 +0100

    try to fix problem with mat-card refs (?)

--- src/app/lazy-testing/lazy-testing.module.ts
+++ src/app/lazy-testing/lazy-testing.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LazyTestingRoutingModule } from './lazy-testing-routing.module';
 import { LazyComponent } from './lazy.component';
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import {MatFormFieldModule} from "@angular/material/form-field";
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule} from "@angular/material";
 import {FormsModule} from "@angular/forms";
 import {IqbComponentsModule} from "../components/iqb-components.module";
-import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
