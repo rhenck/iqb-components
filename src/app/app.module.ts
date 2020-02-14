3a459712100b4129207373211c8fd8b3d76178eb
paflov <paf@titelfrei.de>
2020-02-14 15:24:07 +0100

    added missing dependency fpor unit test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { Showcase } from './showcase.component';
-import {IqbComponentsModule} from './components/iqb-components.module';
+import { IqbComponentsModule } from './components/iqb-components.module';
+import { LazyTestingModule } from './lazy-testing/lazy-testing.module';
 
 import {
     MatButtonModule,
@@ -16,14 +17,14 @@ import {
     MatDialogModule
 } from '@angular/material';
 
-import {FormsModule} from '@angular/forms';
-import {ShowcaseService} from "./showcase.service";
+import { FormsModule } from '@angular/forms';
+import { ShowcaseService } from './showcase.service';
 import { HttpClientModule } from '@angular/common/http';
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {ShowcaseRoutingModule} from "./showcase-routing.module";
-import {HashLocationStrategy, LocationStrategy} from "@angular/common";
-import {AppComponent} from "./app.component";
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShowcaseRoutingModule } from './showcase-routing.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
@@ -46,7 +47,8 @@ import {AppComponent} from "./app.component";
         MatDialogModule,
         HttpClientModule,
         FlexLayoutModule,
-        ShowcaseRoutingModule
+        ShowcaseRoutingModule,
+        LazyTestingModule
     ],
     providers: [
         ShowcaseService,
--- src/app/lazy-testing/lazy.component.spec.ts
+++ src/app/lazy-testing/lazy.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LazyComponent } from './lazy.component';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule, MatRippleModule
+} from '@angular/material';
+import { IqbComponentsModule } from '../components/iqb-components.module';
+import { FormsModule } from '@angular/forms';
+import { ObserversModule } from '@angular/cdk/observers';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LazyComponent', () => {
   let component: LazyComponent;
@@ -8,13 +19,27 @@ describe('LazyComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LazyComponent ]
+      imports: [
+        ObserversModule,
+        RouterTestingModule.withRoutes([]),
+        MatCardModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatInputModule,
+        MatRippleModule,
+        IqbComponentsModule,
+        FormsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [
+        LazyComponent,
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LazyComponent);
+    fixture = TestBed.createComponent<LazyComponent>(LazyComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
